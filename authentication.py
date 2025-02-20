f4ec996ac6ee65a3fabce0a9d92dcac1add8b089
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2025-02-20 22:28:00 +0530

    update

--- authentication.py
+++ authentication.py
@@ -140,4 +140,4 @@ def main():
 
 main()
 
-""" 1. API-key: unique identifier that allows user to interact with database via API (Application Programming Interface). """
+""" 1. API-key: unique identifier that allows user to interact with database via API (Application Programming Interface) """
