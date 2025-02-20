d84ae165960ada027cedf422ab02cfde322db252
Yorkester1814 <168708784+Yorkester1814@users.noreply.github.com>
2025-02-20 22:20:46 +0530

    update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-API_key = "config.json"
+API_key = "C:\\Users\\priya\\Downloads\\fbt1-957b5-firebase-adminsdk-j232s-53ef972615.json"
 cred = credentials.Certificate(API_key)
 firebase_admin.initialize_app(cred)
 fbt = firestore.client()
@@ -25,71 +25,6 @@ class User:
         self.PW = None
         self.FN = None
         self.LN = None
-    
-    def remove(self):
-        print("\nDeleting option enabled")
-        acc = DOCS.document(self.UN)
-        acc.delete()
-        print(f"account {acc.id} successfully deleted\n")
-
-                
-    def edit_account(self):
-        print("\nEditing option enabled")
-        while True:
-            dr = DOCS.document(self.UN)
-            data = dr.get().to_dict()
-            try:            
-                print("1. username")
-                print("2. password")
-                print("3. first name")
-                print("4. last name")
-                E = int(input(f"What do you want to edit in your account: ")) 
-
-                if E == 1:
-                    new_un = str(input("Enter new username: "))
-                    if invalid_name(new_un): 
-                        print("username you provided is of wrong format, provide single-word username")
-                    else:
-                        if not_Unique(new_un):
-                            if new_un == self.UN: 
-                                print("no change in username will occur")
-                            else:
-                                print("username you provided already exist, provide another username")
-                        else:
-                            new_dr = DOCS.document(new_un)
-                            new_dr.set(data)
-                            new_dr.update({"Username": new_un})
-                            dr.delete()
-                            print(f"username & account name renamed to {new_dr.id}")
-
-                elif E == 2:
-                    new_pw = str(input("Enter new password: "))
-                    dr.update({"Password": new_pw})
-                    print(f"password of account '{dr.id}' changed to {new_pw}")
-                
-                elif E == 3:
-                    new_fn = str(input("Enter new first name: "))
-                    if invalid_name(new_fn):
-                        print("first name you provided is of wrong format, provide correct first name")
-                    else:
-                        dr.update({"First_name": new_fn})
-                        print(f"first name of account '{dr.id}' changed to {new_fn}")
-
-                elif E == 4:
-                    new_ln = str(input("Enter new last name: "))
-                    if invalid_name(new_ln):
-                            print("last name you provided is of wrong format, provide correct last name")
-                    else:
-                        dr.update({"Last_name": new_ln})
-                        print(f"last name of account '{dr.id}' changed to {new_ln}")
-                else:
-                    print("editing is completed\n")
-                    break
-            
-                self.UN = new_dr
-            
-            except ValueError:
-                print("Invalid input! Please enter integer")         
 
     def sign_in(self):
         print("\nSign-in option enabled")
@@ -104,29 +39,90 @@ class User:
             if not_Unique(self.UN):
                 print("username already exist. provide unique username\n")
             else:
-                doc_ref = DOCS.document(self.UN)
+                
                 data = {"Username": self.UN, "Password": self.PW, "First_name": self.FN, "Last_name": self.LN}
-                doc_ref.set(data)
-                print(f"Account successfully created with ID: {doc_ref.id}\n")
+                account = DOCS.document()
+                account.set(data)
+                print(f"Account successfully created with ID: {account.id}\n")
 
     def login(self):
         print("\nLog-in option enabled")
         self.UN = str(input("Enter username you want to log-in: "))
         self.PW = str(input("Enter password for the account: "))
-        flag = False
 
+        flag = False
         for doc in docr_stream:
             DOC = doc.to_dict()
             if (DOC["Username"] == self.UN) and (DOC["Password"] == self.PW):
-                print(f"Access to your account {self.UN} is granted\n")
+                print(f"Access to your account {doc.id} is granted\n")
                 flag = True
                 break
 
         if flag == True: 
             modify = str(input("What do you want to do in your account: "))
-            if modify == "edit": self.edit_account()
-            elif modify == "remove": self.remove()
-            else: print("No modification occurs\n")
+
+            if modify == "edit": 
+                print("\nEditing option enabled")
+                while True:
+                    try: 
+                        account = DOCS.document(doc.id) 
+                        print("1. username")
+                        print("2. password")
+                        print("3. first name")
+                        print("4. last name")
+                        E = int(input(f"What do you want to edit in your account: ")) 
+
+                        if E == 1:
+                            new_un = str(input("Enter new username: "))
+                            if invalid_name(new_un): 
+                                print("username you provided is of wrong format, provide single-word username")
+                            else:
+                                if not_Unique(new_un):
+                                    if new_un == self.UN: print("no change in username will occur")
+                                    else: print("username you provided already exist, provide another username")
+                                else:
+                                    self.UN = new_un
+                                    account.update({"Username": new_un})
+                                    print(f"username renamed to {self.UN}")
+
+                        elif E == 2:
+                            new_pw = str(input("Enter new password: "))
+                            self.PW = new_pw
+                            account.update({"Password": self.PW})
+                            print(f"password changed to {self.PW}")
+                
+                        elif E == 3:
+                            new_fn = str(input("Enter new first name: "))
+                            if invalid_name(new_fn):
+                                print("first name you provided is of wrong format, provide correct first name")
+                            else:
+                                self.FN = new_fn
+                                account.update({"First_name": self.FN})
+                                print(f"first name changed to {self.FN}")
+
+                        elif E == 4:
+                            new_ln = str(input("Enter new last name: "))
+                            if invalid_name(new_ln):
+                                print("last name you provided is of wrong format, provide correct last name")
+                            else:
+                                self.LN = new_ln
+                                account.update({"Last_name": self.LN})
+                                print(f"last name changed to {self.LN}")
+                        else:
+                            print("editing is completed\n")
+                            break
+            
+                    except ValueError:
+                        print("Invalid input! Please enter integer")
+
+            elif modify == "remove":
+                print("\nDeleting option enabled") 
+                doc.reference.delete()
+# We've used doc.reference because it provide actual path of document as doc is an object of our data & it can't be used with delete()
+                print(f"account {doc.id} successfully deleted\n")
+
+            else: 
+                print("No modification occurs\n")
 
         else:
             if (DOC["Username"] == self.UN) and (DOC["Password"] != self.PW):
